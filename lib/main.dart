028e26921642de01ab49b9e1ba304246b90f568b
GauravAnand563 <gauravanand563@gmail.com>
2021-05-29 19:07:13 +0530

    merged authentication and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,76 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:rive/rive.dart';
+import 'package:taskify/services/auth.dart';
+import 'package:taskify/views/login.dart';
+import 'package:taskify/views/register.dart';
 import 'package:taskify/views/space/home.dart';
 import 'package:taskify/views/space/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:taskify/vedant_task/slidebar.dart';
+import 'package:taskify/views/splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: SpaceSplashPage.id,
-      routes: {
-        SpaceSplashPage.id: (context) => SpaceSplashPage(),
-        SpaceHomePage.id: (context) => SpaceHomePage(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+              child: Container(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                  child: Text('An Unexpected Error occured! :(')));
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        return Center(
+            child: Container(
+                height: 100, width: 100, child: CircularProgressIndicator()));
       },
     );
   }
 }
 
-// class App extends StatelessWidget {
-//   // final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder(
-//       future: _initialization,
-//       builder: (context, snapshot) {
-//         if (snapshot.hasError) {
-//           return Center(
-//               child: Container(
-//                   height: 100,
-//                   width: MediaQuery.of(context).size.width,
-//                   child: Text('An Unexpected Error occured! :(')));
-//         }
-
-//         if (snapshot.connectionState == ConnectionState.done) {
-//           return MyApp();
-//         }
-
-//         return Center(
-//             child: Container(
-//                 height: 100, width: 100, child: CircularProgressIndicator()));
-//       },
-//     );
-//   }
-// }
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: Colors.black,
-//         body: SafeArea(
-//           child: Center(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 CircleAvatar(
-//                   radius: 100.0,
-//                   backgroundImage: AssetImage('images/logo.png'),
-//                 ),
-//                 SizedBox(
-//                   height: 20.0,
-//                 ),
-//                 Text(
-//                   'taskify',
-//                   style: TextStyle(
-//                     fontSize: 30.0,
-//                     fontWeight: FontWeight.bold,
-//                     color: Colors.white,
-//                   ),
-//                 ),
-//                 Divider(
-//                   height: 10.0,
-//                   color: Colors.blue,
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class MyApp extends StatelessWidget {
+  final _firebaseAuthUser = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    final _auth = Auth(firebaseAuth: _firebaseAuthUser);
+    return MultiProvider(
+      providers: [
+        Provider<Auth>(
+          create: (context) => _auth,
+        ),
+        // ChangeNotifierProvider<TabViews>(
+        //   create: (context) => TabViews(),
+        // ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: SplashPage.id,
+        routes: {
+          SplashPage.id: (context) => SplashPage(),
+          RegisterPage.id: (context) => RegisterPage(),
+          LoginPage.id: (context) => LoginPage(),
+          SpaceSplashPage.id: (context) => SpaceSplashPage(),
+          SpaceHomePage.id: (context) => SpaceHomePage(),
+        },
+      ),
+    );
+  }
+}
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -2,13 +2,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:provider/provider.dart';
+import 'package:taskify/services/auth.dart';
+import 'package:taskify/views/register.dart';
 
-class login extends StatefulWidget {
+class LoginPage extends StatefulWidget {
+  static String id = 'loginPage';
   @override
-  _loginState createState() => _loginState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _loginState extends State<login> {
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  bool _showProgressBar = false;
   bool isRememberMe = false;
   Widget buildEmail() {
     return Column(
@@ -38,6 +46,7 @@ class _loginState extends State<login> {
               ]),
           height: 60.0,
           child: TextField(
+            controller: _emailController,
             keyboardType: TextInputType.emailAddress,
             style: TextStyle(
               color: Colors.black,
@@ -87,6 +96,7 @@ class _loginState extends State<login> {
               ]),
           height: 60.0,
           child: TextField(
+            controller: _passwordController,
             obscureText: true,
             style: TextStyle(
               color: Colors.black,
@@ -111,9 +121,9 @@ class _loginState extends State<login> {
   Widget buildForgotPassBtn() {
     return Container(
       alignment: Alignment.centerRight,
-      child: FlatButton(
+      padding: EdgeInsets.only(right: 0.0),
+      child: TextButton(
         onPressed: () => print("Forgot Password Pressed"),
-        padding: EdgeInsets.only(right: 0.0),
         child: Text(
           'Forgot Password',
           style: TextStyle(
@@ -161,7 +171,25 @@ class _loginState extends State<login> {
       width: double.infinity,
       child: RaisedButton(
         elevation: 5,
-        onPressed: () => print("Login Button Pressed"),
+        onPressed: () async {
+          final auth = Provider.of<Auth>(context, listen: false);
+          setState(() {
+            _showProgressBar = true;
+          });
+          var result = await auth.signIn(
+              context: context,
+              email: _emailController.text.toString().trim(),
+              password: _passwordController.text.toString().trim());
+          setState(() {
+            _showProgressBar = false;
+          });
+          if (result)
+            print(result);
+          // Navigator.popAndPushNamed(context, HomePage.id);
+          else {
+            _passwordController.clear();
+          }
+        },
         padding: EdgeInsets.all(15.0),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
@@ -181,7 +209,7 @@ class _loginState extends State<login> {
 
   Widget buildSignUpBtn() {
     return GestureDetector(
-      onTap: () => Navigator.pushNamed(context, '/third'),
+      onTap: () => Navigator.pushNamed(context, RegisterPage.id),
       child: RichText(
         text: TextSpan(children: [
           TextSpan(
@@ -211,53 +239,62 @@ class _loginState extends State<login> {
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: GestureDetector(
-          child: Stack(
-            children: [
-              Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Color(0x665ac18e),
-                          Color(0x995ac18e),
-                          Color(0xcc5ac18e),
-                          Color(0xff5ac18e),
-                        ]),
-                  ),
-                  child: SingleChildScrollView(
-                    physics: AlwaysScrollableScrollPhysics(),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 25.0, vertical: 120.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Sign In',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 50,
-                        ),
-                        buildEmail(),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        buildPassword(),
-                        buildForgotPassBtn(),
-                        buildRememberCb(),
-                        buildLoginBtn(),
-                        buildSignUpBtn(),
-                      ],
+          child: ModalProgressHUD(
+            inAsyncCall: _showProgressBar,
+            child: Stack(
+              children: [
+                Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: Image.asset('assets/images/bg.jpg').image,
+                        colorFilter: new ColorFilter.mode(
+                            Colors.black.withOpacity(0.3), BlendMode.dstATop),
+                        fit: BoxFit.cover,
+                      ),
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color(0x665ac18e),
+                            Color(0x995ac18e),
+                            Color(0xcc5ac18e),
+                            Color(0xff5ac18e),
+                          ]),
                     ),
-                  )),
-            ],
+                    child: SingleChildScrollView(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 25.0, vertical: 120.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Sign In',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 50,
+                          ),
+                          buildEmail(),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          buildPassword(),
+                          buildForgotPassBtn(),
+                          buildRememberCb(),
+                          buildLoginBtn(),
+                          buildSignUpBtn(),
+                        ],
+                      ),
+                    )),
+              ],
+            ),
           ),
         ),
       ),
